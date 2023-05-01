036777a889d0b3b9ad84f66b1011bd0844d89ae2
Jack Roudebush <roudebushjack@gmail.com>
2023-04-30 20:55:03 -0400

    Create display_board method in GameBoard class

--- game_board.rb
+++ game_board.rb
@@ -1,3 +1,23 @@
 class GameBoard
+  
+  @one = '1'
+  @two = '2'
+  @three = '3'
+  @four = '4'
+  @five = '5'
+  @six = '6'
+  @seven = '7'
+  @eight = '8'
+  @nine = '9'
 
+  def display_board
+    p '|=====================|'
+    p '|=====TIC TAC TOE=====|'
+    p '|=====================|'
+    p ' #{one} | #{two} | #{three} '
+    p '---+---+---'
+    p ' #{four} | #{five} | #{six} '
+    p '---+---+---'
+    p ' #{seven} | #{eight} | #{nine} '
+  end
 end
