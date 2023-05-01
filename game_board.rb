7a518311542cd3e2ddfdd01b7fedbad206c1beb0
Jack Roudebush <roudebushjack@gmail.com>
2023-04-30 21:12:17 -0400

    Align display_board output

--- game_board.rb
+++ game_board.rb
@@ -1,23 +1,26 @@
 class GameBoard
-  
-  @one = '1'
-  @two = '2'
-  @three = '3'
-  @four = '4'
-  @five = '5'
-  @six = '6'
-  @seven = '7'
-  @eight = '8'
-  @nine = '9'
+
+  def intitialize
+  end
 
   def display_board
-    p '|=====================|'
-    p '|=====TIC TAC TOE=====|'
-    p '|=====================|'
-    p ' #{one} | #{two} | #{three} '
-    p '---+---+---'
-    p ' #{four} | #{five} | #{six} '
-    p '---+---+---'
-    p ' #{seven} | #{eight} | #{nine} '
+    @one = '1'
+    @two = '2'
+    @three = '3'
+    @four = '4'
+    @five = '5'
+    @six = '6'
+    @seven = '7'
+    @eight = '8'
+    @nine = '9'
+
+    puts '|=====================|'
+    puts '|=====TIC TAC TOE=====|'
+    puts '|=====================|'
+    puts "       #{@one} | #{@two} | #{@three} "
+    puts '      ---+---+---'
+    puts "       #{@four} | #{@five} | #{@six} "
+    puts '      ---+---+---'
+    puts "       #{@seven} | #{@eight} | #{@nine} "
   end
 end
---
+++ ttt.rb
@@ -0,0 +1,4 @@
+require_relative 'game_board'
+board = GameBoard.new
+
+board.display_board
